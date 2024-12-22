57f26621c140129636a6a325add8c21c8fc0c018
solovikas <solovvika2005@gmail.com>
2024-12-23 00:43:17 +0300

    Modified server/routes/genreRouter.js

--- server/routes/genreRouter.js
+++ server/routes/genreRouter.js
@@ -4,6 +4,8 @@ const genreController = require("../controllers/genreController");
 const checkRole = require("../middleware/checkRoleMiddleware");
 
 router.post("/", checkRole("ADMIN"), genreController.create);
+router.post("/addphoto", checkRole("ADMIN"), genreController.addPhoto);
+router.post("/addinfo", checkRole("ADMIN"), genreController.addInfo);
 router.get("/", genreController.getAll);
 router.get("/:id", genreController.getCertain);
 
